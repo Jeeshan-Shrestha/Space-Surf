983e880cffa1251b05ec1f9d36ae848215134666
Sanskar-Rijal <rijalsanskar@gmail.com>
2026-03-23 20:57:52 +0545

    adding score board

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,10 +7,27 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int ScorePerHit=15;
+
+    ScoreBoard scoreBoard;
+
+    private void Start() {
+        //Look through all GameObjects in the scene and find the one that has the ScoreBoard script attached.
+       scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
     
-    private void OnParticleCollision(GameObject other) {
-       GameObject vfx =  Instantiate(deathVFX, transform.position, Quaternion.identity); //Quaternion.identiy menas we dont need rotation
-       vfx.transform.parent = parent; //putting the vfx in the parent object to keep the hierarchy clean
+    private void OnParticleCollision(GameObject other)
+    {
+        EnemyKilled();
+        //Increase Score when enemy is killed;
+        scoreBoard.IncreaseScore(ScorePerHit);
+
+    }
+
+    private void EnemyKilled()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity); //Quaternion.identiy menas we dont need rotation
+        vfx.transform.parent = parent; //putting the vfx in the parent object to keep the hierarchy clean
         Destroy(gameObject);
     }
 }
